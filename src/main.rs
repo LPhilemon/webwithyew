739291d723c32b246cc563cebd221003eb8fff2d
Philemon <72923339+lphil-coder@users.noreply.github.com>
2023-05-02 12:31:57 +0300

    [Integrated APIs,     gloo-net For making the fetch call.     serde with derive features For de-serializing the JSON response     wasm-bindgen-futures For executing Rust Future as a Promise]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use yew::prelude::*;
+use serde::Deserialize;
 
-#[derive(Clone, PartialEq)]
+#[derive(Clone, PartialEq, Deserialize)]
 struct Video {
     id: usize,
     title: String,
@@ -27,7 +28,7 @@ fn videos_list(VideosListProps { videos, on_click}: &VideosListProps) -> Html {
                      };
 
         html! {
-        <p key={video.id onclick={on_video_select}}>{format!("{}: {}", video.speaker, video.title)}</p>
+        <p key={video.id} onclick={on_video_select}>{format!("{}: {}", video.speaker, video.title)}</p>
     }}
 ).collect()
 }
@@ -47,6 +48,8 @@ fn video_details(VideosDetailsProps { video }: &VideosDetailsProps) -> Html {
     }
 }
 
+use gloo_net::http::Request;
+
 #[function_component(App)]
 fn app() -> Html {
 
@@ -63,41 +66,58 @@ fn app() -> Html {
                  <VideoDetails video={video.clone()} />
              });
 
-    let videos = vec![
-        Video {
-            id: 1,
-            title: "Building and breaking things".to_string(),
-            speaker: "John Doe".to_string(),
-            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
-        },
-        Video {
-            id: 2,
-            title: "The development process".to_string(),
-            speaker: "Jane Smith".to_string(),
-            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
-        },
-        Video {
-            id: 3,
-            title: "The Web 7.0".to_string(),
-            speaker: "Matt Miller".to_string(),
-            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
-        },
-        Video {
-            id: 4,
-            title: "Mouseless development".to_string(),
-            speaker: "Tom Jerry".to_string(),
-            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
-        },
-    ];
+    // let videos = vec![
+    //     Video {
+    //         id: 1,
+    //         title: "Building and breaking things".to_string(),
+    //         speaker: "John Doe".to_string(),
+    //         url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+    //     },
+    //     Video {
+    //         id: 2,
+    //         title: "The development process".to_string(),
+    //         speaker: "Jane Smith".to_string(),
+    //         url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+    //     },
+    //     Video {
+    //         id: 3,
+    //         title: "The Web 7.0".to_string(),
+    //         speaker: "Matt Miller".to_string(),
+    //         url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+    //     },
+    //     Video {
+    //         id: 4,
+    //         title: "Mouseless development".to_string(),
+    //         speaker: "Tom Jerry".to_string(),
+    //         url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+    //     },
+    // ];
 
-  
+    let videos = use_state(|| vec![]);
+         {
+             let videos = videos.clone();
+             use_effect_with_deps(move |_| {
+                 let videos = videos.clone();
+                 wasm_bindgen_futures::spawn_local(async move {
+                    let fetched_videos: Vec<Video> = Request::get("/tutorial/data.json")
+                         .send()
+                         .await
+                         .unwrap()
+                         .json()
+                         .await
+                         .unwrap();
+                     videos.set(fetched_videos);
+                 });
+                 || ()
+             }, ());
+         }
 
     html! {
         <>
             <h1>{ "RustConf Explorer" }</h1>
             <div>
                 <h3>{"Videos to watch"}</h3>
-                <VideosList videos={videos} on_click={on_video_select.clone()} />
+                <VideosList videos={(*videos).clone()} on_click={on_video_select.clone()} />
             </div>
             {for details}
             <div>
